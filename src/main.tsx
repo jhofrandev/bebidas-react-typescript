7afff2594ccdf5b53fb32aace2a922fcddd4ffdb
Jhofran Briceño <jhofran.dev@outlook.com>
2025-06-29 21:50:41 -0500

    add changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AppRouter from './router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppRouter from "./router";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppRouter />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// ADD THIS
