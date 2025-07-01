7c438dbc268e66283009c82b4e3f76743201b478
Jhofran Briceño <jhofran.dev@outlook.com>
2025-07-01 16:51:57 -0500

    feat: implement favorites functionality with favorites slice and update modal for favorite management

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,6 +13,8 @@ export default function Modal() {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+  const favoriteExists = useAppStore((state) => state.favoriteExists);
+  const handleClickFavorite = useAppStore((state) => state.handleClickFavorite);
 
   const renderIngredients = () => {
     const ingredients: React.ReactElement[] = [];
@@ -101,8 +103,11 @@ export default function Modal() {
                     <button
                       type="button"
                       className="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
+                      onClick={() => handleClickFavorite(selectedRecipe)}
                     >
-                      Agregar a Favoritos
+                      {favoriteExists(selectedRecipe.idDrink)
+                        ? "Eliminar Favorito"
+                        : "Agregar a Favoritos"}
                     </button>
                   </div>
                 </DialogPanel>
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,14 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
+import {
+  createFavoritesSlice,
+  type FavoritesSliceType,
+} from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
